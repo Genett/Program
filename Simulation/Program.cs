e6c1bf2470b02f8c56ed34946cec001a897a325b
Nick <salms.nikita@yandex.ru>
2015-06-20 18:59:07 +0300

    Upload

---
+++ Simulation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Simulation
+{
+	class Program
+	{
+		private static Simulation simulation;
+
+		static void Main()
+		{
+			// Welcome message
+			Console.WriteLine(
+				@"Welcome to THE SIMULATION
+
+Commands:
+1. ""run"" - Run the simulation (A 'number' variable will increment each second)
+2. ""stop"" - Stop the simulation
+3. ""print_number"" - Print the 'number' variable
+4. ""clean_number"" - Clean the 'number' variable
+==================================================
+");
+
+			// Start simulation
+			simulation = new Simulation();
+
+			// Input commands
+			while(true)
+			{
+				string input = Console.ReadLine();
+				Command(input);
+			}
+		}
+
+		static void Command(string cmd)
+		{
+			switch(cmd)
+			{
+			case "run":
+				simulation.Run();
+			break;
+
+			case "stop":
+				simulation.Stop();
+			break;
+
+			case "print_number":
+				Console.WriteLine(simulation.number);
+			break;
+
+			case "clean_number":
+				simulation.number = 0;
+				Console.WriteLine(simulation.number);
+			break;
+
+			default:
+				Console.WriteLine("\"" + cmd + "\" command not found");
+			break;
+			}
+		}
+	}
+}
---
+++ Simulation/Simulation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace Simulation
+{
+	public class Simulation
+	{
+		public int number; // Main variable
+
+
+		/* States of The Simulation */
+		private enum State
+		{
+			Running, // Running state
+			Stopped // Stopped state
+		}
+
+		private State currentState; // Current state
+
+		public Simulation()
+		{
+			Thread thread = new Thread(Live); // Create a new thread
+			thread.Start();
+
+			number = 0;
+
+			// Default state
+			currentState = State.Stopped;
+		}
+			
+		private void Live()
+		{
+			while(true)
+			{
+				if (currentState == State.Running)
+				{
+					number++;
+					Thread.Sleep(1000); // Delay for 1 second
+				}
+			}
+		}
+
+		public void Run()
+		{
+			currentState = State.Running;
+
+			Console.WriteLine("Done.");
+		}
+
+		public void Stop()
+		{
+			currentState = State.Stopped;
+
+			Console.WriteLine("Done.");
+		}
+	}
+}
